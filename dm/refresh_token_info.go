091d9d24e7c69e1c008ad70f12fae5630e78db7d
waldemar <hellodoge@tuta.io>
2021-06-16 21:14:53 +0300

    add ID field to RefreshTokenInfo datatype

--- dm/refresh_token_info.go
+++ dm/refresh_token_info.go
@@ -1,10 +1,11 @@
 package dm
 
 type RefreshTokenInfo struct {
-	Token       string `json:"-" db:"token"`
-	UserID      int    `json:"-" db:"user_id"`
-	IP          string `json:"ip" db:"ip_address"`
-	IssuedAt    string `json:"issued-at" db:"issued_at"`
-	ExpiresAt   string `json:"expires-at" db:"expires_at"`
+	ID          int    `json:"id"          db:"id"`
+	Token       string `json:"-"           db:"token"`
+	UserID      int    `json:"-"           db:"user_id"`
+	IP          string `json:"ip"          db:"ip_address"`
+	IssuedAt    string `json:"issued-at"   db:"issued_at"`
+	ExpiresAt   string `json:"expires-at"  db:"expires_at"`
 	Invalidated bool   `json:"invalidated" db:"invalidated"`
 }
